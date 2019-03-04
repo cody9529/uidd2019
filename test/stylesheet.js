70964d1411f36dc137ac85f6f7cb82706e834f12
TingWei <abc33131@gmail.com>
2019-03-04 16:05:51 +0800

    a

--- test/stylesheet.js
+++ test/stylesheet.js
@@ -1,14 +1,11 @@
-// Allow multiple draggable items
+
+let sourceContainerId = ''
+
 let dragSources = document.querySelectorAll('[draggable="true"]')
 dragSources.forEach(dragSource => {
   dragSource.addEventListener('dragstart', dragStart)
 })
 
-function dragStart (e) {
-  e.dataTransfer.setData('text/plain', e.target.id)
-}
-
-// Allow multiple dropped targets
 let dropTargets = document.querySelectorAll('[data-role="drag-drop-container"]')
 dropTargets.forEach(dropTarget => {
   dropTarget.addEventListener('drop', dropped)
@@ -16,14 +13,23 @@ dropTargets.forEach(dropTarget => {
   dropTarget.addEventListener('dragover', cancelDefault)
 })
 
-function dropped (e) {
-  cancelDefault(e)
-  let id = e.dataTransfer.getData('text/plain')
-  e.target.appendChild(document.querySelector('#' + id))
-}
-
 function cancelDefault (e) {
   e.preventDefault()
   e.stopPropagation()
   return false
 }
+
+function dragStart (e) {
+  e.dataTransfer.setData('text/plain', e.target.id)
+  sourceContainerId = this.parentElement.id
+  console.log('sourceContainerId', sourceContainerId)
+}
+
+function dropped (e) {
+  if (this.id !== sourceContainerId) {
+    cancelDefault(e)
+    let id = e.dataTransfer.getData('text/plain')
+    e.target.appendChild(document.querySelector('#' + id))
+  }
+}
+
