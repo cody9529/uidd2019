845faf79b4f61fac0da31dc84682732e56174425
TingWei <abc33131@gmail.com>
2019-03-07 08:20:38 +0800

    go

--- test/stylesheet.js
+++ test/stylesheet.js
@@ -1,35 +1,55 @@
 
-let sourceContainerId = ''
+let sourceContainerId = "";
 
-let dragSources = document.querySelectorAll('[draggable="true"]')
+// Allow multiple draggable items
+let dragSources = document.querySelectorAll('[draggable="true"]');
 dragSources.forEach(dragSource => {
-  dragSource.addEventListener('dragstart', dragStart)
-})
+    dragSource.addEventListener("dragstart", dragStart);
+    dragSource.addEventListener("dragend", dragEnd);
+});
 
-let dropTargets = document.querySelectorAll('[data-role="drag-drop-container"]')
+function dragStart(e) {
+    this.classList.add("dragging");
+    e.dataTransfer.setData("text/plain", e.target.id);
+    sourceContainerId = this.parentElement.id;
+}
+
+function dragEnd(e) {
+    this.classList.remove("dragging");
+}
+
+// Allow multiple dropped targets
+let dropTargets = document.querySelectorAll(
+    '[data-role="drag-drop-container"]'
+);
 dropTargets.forEach(dropTarget => {
-  dropTarget.addEventListener('drop', dropped)
-  dropTarget.addEventListener('dragenter', cancelDefault)
-  dropTarget.addEventListener('dragover', cancelDefault)
-})
+    dropTarget.addEventListener("drop", dropped);
+    dropTarget.addEventListener("dragenter", cancelDefault);
+    dropTarget.addEventListener("dragover", dragOver);
+    dropTarget.addEventListener("dragleave", dragLeave);
+});
 
-function cancelDefault (e) {
-  e.preventDefault()
-  e.stopPropagation()
-  return false
+function dropped(e) {
+    // execute function only when target container is different from source container
+    if (this.id !== sourceContainerId) {
+        cancelDefault(e);
+        let id = e.dataTransfer.getData("text/plain");
+        e.target.appendChild(document.querySelector("#" + id));
+        this.classList.remove("hover");
+    }
 }
 
-function dragStart (e) {
-  e.dataTransfer.setData('text/plain', e.target.id)
-  sourceContainerId = this.parentElement.id
-  console.log('sourceContainerId', sourceContainerId)
+function dragOver(e) {
+    cancelDefault(e);
+    this.classList.add("hover");
 }
 
-function dropped (e) {
-  if (this.id !== sourceContainerId) {
-    cancelDefault(e)
-    let id = e.dataTransfer.getData('text/plain')
-    e.target.appendChild(document.querySelector('#' + id))
-  }
+function dragLeave(e) {
+    this.classList.remove("hover");
 }
 
+function cancelDefault(e) {
+    e.preventDefault();
+    e.stopPropagation();
+    return false;
+}
