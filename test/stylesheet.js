220f4445424fd7829fa37d6bd3e0c269f0a2c23e
TingWei <abc33131@gmail.com>
2019-03-18 10:17:56 +0800

    ewq

--- test/stylesheet.js
+++ test/stylesheet.js
@@ -45,6 +45,27 @@ interact('.drag-drop')
         elementRect: { top: 0, left: 0, bottom: 1, right: 1 }
       })
     ],
-    autoScroll: true
+    autoScroll: true;
     // dragMoveListener from the dragging demo above
+    onmove: dragMoveListener
   });
+
+
+function dragMoveListener (event) {
+    var target = event.target,
+        // keep the dragged position in the data-x/data-y attributes
+        x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
+        y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
+
+    // translate the element
+    target.style.webkitTransform =
+    target.style.transform =
+      'translate(' + x + 'px, ' + y + 'px)';
+
+    // update the posiion attributes
+    target.setAttribute('data-x', x);
+    target.setAttribute('data-y', y);
+  }
+
+  // this is used later in the resizing and gesture demos
+  window.dragMoveListener = dragMoveListener;
