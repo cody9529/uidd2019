98863a96da7a4d9abbc8053e98b37a32dd45a1e8
TingWei <abc33131@gmail.com>
2019-03-18 09:09:25 +0800

    ewq

--- test/stylesheet.js
+++ test/stylesheet.js
@@ -1,12 +1,51 @@
 
-function AllowDrop(event){
-    event.preventDefault();
-}
-function Drag(event){
-    event.dataTransfer.setData("text",event.currentTarget.id);
-}
-function Drop(event){
-    event.preventDefault();
-    var data=event.dataTransfer.getData("text");
-    event.currentTarget.appendChild(document.getElementById(data));
-}
+interact('.dropzone').dropzone({
+  // only accept elements matching this CSS selector
+  // Require a 75% element overlap for a drop to be possible
+  overlap: 0.75,
+
+  // listen for drop related events:
+
+  ondropactivate: function (event) {
+    // add active dropzone feedback
+    event.target.classList.add('drop-active')
+  },
+  ondragenter: function (event) {
+    var draggableElement = event.relatedTarget;
+    var dropzoneElement = event.target;
+
+    // feedback the possibility of a drop
+    dropzoneElement.classList.add('drop-target')
+    draggableElement.classList.add('can-drop')
+    draggableElement.textContent = 'Dragged in'
+  },
+  ondragleave: function (event) {
+    // remove the drop feedback style
+    event.target.classList.remove('drop-target')
+    event.relatedTarget.classList.remove('can-drop')
+    event.relatedTarget.textContent = 'Dragged out'
+  },
+  ondrop: function (event) {
+    event.relatedTarget.textContent = 'Dropped'
+  },
+  ondropdeactivate: function (event) {
+    // remove active dropzone feedback
+    event.target.classList.remove('drop-active')
+    event.target.classList.remove('drop-target')
+  }
+});
+
+interact('.drag-drop')
+  .draggable({
+    inertia: true,
+    modifiers: [
+      interact.modifiers.restrict({
+        restriction: "parent",
+        endOnly: true,
+        elementRect: { top: 0, left: 0, bottom: 1, right: 1 }
+      })
+    ],
+    autoScroll: true,
+    // dragMoveListener from the dragging demo above
+    onmove: dragMoveListener
+  });
