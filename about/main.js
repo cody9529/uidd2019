9352b6c3c639a7207d7f4f9bbbda09fd0ea94e74
TingWei <abc33131@gmail.com>
2019-04-16 11:13:34 +0800

    fgds

--- about/main.js
+++ about/main.js
@@ -1,16 +1,17 @@
-  $(document).ready(function() {
-    //滑鼠切換底色
-    $('.picback').each(function()
-      //class名為demo的每一個(each)執行以下function
-    {
-        $(this).mouseover(function(){
-            $(this).src="man_工作區域 1.jpg";  
-            //被mouseover(被滑鼠滑過的.demo) 其css的background-color改為#33648B，文字顏色為#FFFFFF 
-        });
-
-        $(this).mouseout(function(){ 
-            $(this).src="man-06.jpg";           
-            //被mouseout(被滑鼠移出的.demo) 其css的background-color無設定，文字顏色為#000000 
-        });
-    }) ;
+$(document).ready(()=>{
+    $("#img1").hover(
+        ()=>{            
+            // $("#person1").show();
+            $("#img1 img").fadeOut(500,() => {
+                $("#img1 img").attr("src","./man_工作區域 1.jpg").fadeIn(500);
+            });
+          
+        },
+        ()=>{        
+            // $("#intro").show();
+            $("#img1 img").fadeOut(500,() => {
+                $("#img1 img").attr("src","man-06.jpg").fadeIn(500);
+            }); 
+        }
+    );
 })
