13ed9b073d56eba92458f19af2d3130c42f9d067
TingWei <abc33131@gmail.com>
2019-04-16 14:14:02 +0800

    rweq

--- aaa/main.js
+++ aaa/main.js
@@ -2,13 +2,13 @@ $(document).ready(()=>{
     $("#img1").hover(
         ()=>{ 
             $("#img1 img").fadeOut(300,() => {
-                $("#img1 img").attr("src","./man_工作區域 1.jpg").fadeIn(300);
+                $("#img1 img").attr("src","./src/man_工作區域 1.jpg").fadeIn(300);
             });          
                 
         },
         ()=>{   
             $("#img1 img").fadeOut(300,() => {
-                $("#img1 img").attr("src","./man-06.jpg").fadeIn(300);
+                $("#img1 img").attr("src","./src/man-06.jpg").fadeIn(300);
             });      
                   
         })
