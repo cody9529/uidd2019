3d5655395c1c1e3357ced270261ca7309a235291
TingWei <abc33131@gmail.com>
2019-04-16 11:34:25 +0800

    trew

--- about/main.js
+++ about/main.js
@@ -1,17 +1,10 @@
 $(document).ready(()=>{
     $("#img1").hover(
         ()=>{            
-            // $("#person1").show();
-            $("#img1 img").fadeOut(500,() => {
-                $("#img1 img").attr("src","./man_工作區域 1.jpg").fadeIn(500);
-            });
-          
+            $("#img1 img").fadeOut(500)          
         },
         ()=>{        
-            // $("#intro").show();
-            $("#img1 img").fadeOut(500,() => {
-                $("#img1 img").attr("src","man-06.jpg").fadeIn(500);
-            }); 
-        }
-    );
-})
+            $("#img1 img").fadeIn(500) 
+        
+        }); 
+});
