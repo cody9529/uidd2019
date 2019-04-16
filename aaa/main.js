04e4dfc4fb2502df0d563a155ed06bd4451a3c4b
TingWei <abc33131@gmail.com>
2019-04-16 14:33:16 +0800

    rewq

--- aaa/main.js
+++ aaa/main.js
@@ -1,10 +1,7 @@
 $(document).ready(()=>{
     $("#img1").hover(
         ()=>{ 
-            $("#img1 img").fadeOut(300,() => {
-                $("#img1 img").attr("src","./src/man_工作區域 1.jpg").fadeIn(300);
-            });          
-                
+          $("#img1 img").attr("src","./src/man_工作區域 1.jpg").fadeIn(300);                          
         },
         ()=>{   
             $("#img1 img").fadeOut(300,() => {
