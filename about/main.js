a00be7747caa2cf17c68caa5c8e1ae1a22b03cb7
TingWei <abc33131@gmail.com>
2019-04-16 10:52:18 +0800

    rew

--- about/main.js
+++ about/main.js
@@ -1,17 +1,16 @@
-$(document).ready(()=>{
-    $("#pica").hover(
-        ()=>{            
-            // $("#person1").show();
-            $("#pica img").fadeOut(500,() => {
-                $("#pica img").attr("src","./man_工作區域 1.jpg").fadeIn(500);
-            });
-          
-        },
-        ()=>{        
-            // $("#intro").show();
-            $("#pica img").fadeOut(500,() => {
-                $("#pica img").attr("src","./man-06.jpg").fadeIn(500);
-            }); 
-          
-        }
-    )
+  $(document).ready(function() {
+    //滑鼠切換底色
+    $('.picback').each(function()
+      //class名為demo的每一個(each)執行以下function
+    {
+        $(this).mouseover(function(){
+            $(this).src="man_工作區域 1.jpg";  
+            //被mouseover(被滑鼠滑過的.demo) 其css的background-color改為#33648B，文字顏色為#FFFFFF 
+        });
+
+        $(this).mouseout(function(){ 
+            $(this).src="man-06.jpg";           
+            //被mouseout(被滑鼠移出的.demo) 其css的background-color無設定，文字顏色為#000000 
+        });
+    }) ;
+});
