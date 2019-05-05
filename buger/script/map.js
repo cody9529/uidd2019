d178b946aeba7783a8e78245a390d40b4b2621e4
cody9529 <47812572+cody9529@users.noreply.github.com>
2019-05-06 00:04:41 +0800

    Update map.js

--- buger/script/map.js
+++ buger/script/map.js
@@ -12,7 +12,7 @@ function initMap() {
     	var directionsDisplay = new google.maps.DirectionsRenderer();
         var contentString1 = "台南車站店&emsp;</br>";
 	var contentString2 = "林森店&emsp;</br>";
-	var sss = "</br>"
+	var sss = "&emsp;</br>"
 	    
 	var latlng = { lat: 25.046891, lng: 121.516602 }; // 給一個初始位置
         var map = new google.maps.Map(document.getElementById('mapbox'), {
