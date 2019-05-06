b7fc5c4e7c052fc0b539f898fe544c796beb0c4b
cody9529 <47812572+cody9529@users.noreply.github.com>
2019-05-06 23:57:04 +0800

    Update map.js

--- buger111/script/map.js
+++ buger111/script/map.js
@@ -151,6 +151,23 @@ function myFunction1(){
 	var directionsService = new google.maps.DirectionsService();
     	var directionsDisplay = new google.maps.DirectionsRenderer();
 	directionsDisplay.setMap(map);
+	if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(function(position) {
+                var pos = {
+                    lat: position.coords.latitude,
+                    lng: position.coords.longitude
+                };
+		// 路線相關設定
+		var request1 = {
+       	 	    origin:  { lat: position.coords.latitude, lng: position.coords.longitude },
+        	    destination: { lat: 22.997322, lng: 120.212076 },
+        	    travelMode: 'DRIVING'
+    		};
+		var request2 = {
+       	 	    origin:  { lat: position.coords.latitude, lng: position.coords.longitude },
+        	    destination: { lat: 22.988608, lng: 120.224096 },
+        	    travelMode: 'DRIVING'
+    		};
 	directionsService.route(request1, function (result, status) {
         if (status == 'OK') {
             // 回傳路線上每個步驟的細節
@@ -181,6 +198,23 @@ function myFunction2(){
 	var directionsService = new google.maps.DirectionsService();
     	var directionsDisplay = new google.maps.DirectionsRenderer();
 	directionsDisplay.setMap(map);
+		    if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(function(position) {
+                var pos = {
+                    lat: position.coords.latitude,
+                    lng: position.coords.longitude
+                };
+		// 路線相關設定
+		var request1 = {
+       	 	    origin:  { lat: position.coords.latitude, lng: position.coords.longitude },
+        	    destination: { lat: 22.997322, lng: 120.212076 },
+        	    travelMode: 'DRIVING'
+    		};
+		var request2 = {
+       	 	    origin:  { lat: position.coords.latitude, lng: position.coords.longitude },
+        	    destination: { lat: 22.988608, lng: 120.224096 },
+        	    travelMode: 'DRIVING'
+    		};
 	directionsService.route(request1, function (result, status) {
         if (status == 'OK') {
             // 回傳路線上每個步驟的細節
